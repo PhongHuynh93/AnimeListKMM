ee997cadcdc4c3ef8df8db96281aad36fb6f5a71
Phong Huynh <phonghdt@vng.com.vn>
2020-10-16 07:48:42 +0700

    update coroutines library

--- shared/src/androidMain/kotlin/com/wind/animelist/shared/base/Dispatchers.kt
+++ shared/src/androidMain/kotlin/com/wind/animelist/shared/base/Dispatchers.kt
@@ -4,4 +4,3 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 actual val mainDispatcher: CoroutineDispatcher = Dispatchers.Main
-actual val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
--- shared/src/iosMain/kotlin/com/wind/animelist/shared/base/BaseViewModel.kt
+++ shared/src/iosMain/kotlin/com/wind/animelist/shared/base/BaseViewModel.kt
@@ -1,15 +1,21 @@
 package com.wind.animelist.shared.base
 
+import com.wind.animelist.shared.di.bgDispatcherTag
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelChildren
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import org.koin.core.qualifier.named
 
 /**
  * Created by Phong Huynh on 10/6/2020
  */
-actual open class BaseViewModel actual constructor() {
+actual open class BaseViewModel actual constructor(): KoinComponent {
     private val viewModelJob = SupervisorJob()
-    val viewModelScope: CoroutineScope = CoroutineScope(ioDispatcher + viewModelJob)
+    private val ioDispatcher: CoroutineDispatcher by inject(qualifier = named(bgDispatcherTag))
+    private val viewModelScope: CoroutineScope = CoroutineScope(ioDispatcher + viewModelJob)
 
     actual val clientScope: CoroutineScope = viewModelScope
 

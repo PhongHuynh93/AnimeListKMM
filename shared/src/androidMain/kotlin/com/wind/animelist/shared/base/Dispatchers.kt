ef806ac842b9e633c85d7a765ca1110127ca648a
Phong Huỳnh <p7157568@gmail.com>
2020-10-17 16:07:26 +0700

    can run the ui on ios

--- shared/src/androidMain/kotlin/com/wind/animelist/shared/base/Dispatchers.kt
+++ shared/src/androidMain/kotlin/com/wind/animelist/shared/base/Dispatchers.kt
@@ -4,4 +4,4 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 actual val mainDispatcher: CoroutineDispatcher = Dispatchers.Main
-actual val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+actual val ioDispatcher: CoroutineDispatcher = Dispatchers.Default
--- shared/src/iosMain/kotlin/com/wind/animelist/shared/base/BaseViewModel.kt
+++ shared/src/iosMain/kotlin/com/wind/animelist/shared/base/BaseViewModel.kt
@@ -1,7 +1,6 @@
 package com.wind.animelist.shared.base
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelChildren
 
@@ -9,16 +8,12 @@ import kotlinx.coroutines.cancelChildren
  * Created by Phong Huynh on 10/6/2020
  */
 actual open class BaseViewModel actual constructor() {
-//    private val viewModelJob = SupervisorJob()
-//    val viewModelScope: CoroutineScope = CoroutineScope(ioDispatcher + viewModelJob)
-//
-//    actual val clientScope: CoroutineScope = viewModelScope
+    private val viewModelJob = SupervisorJob()
+    val viewModelScope: CoroutineScope = CoroutineScope(ioDispatcher + viewModelJob)
 
-//    actual val clientScope = GlobalScope
+    actual val clientScope: CoroutineScope = viewModelScope
+
     protected actual open fun onCleared() {
-//        viewModelJob.cancelChildren()
+        viewModelJob.cancelChildren()
     }
-
-    actual val clientScope: CoroutineScope
-        get() = GlobalScope
 }
